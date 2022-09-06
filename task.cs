311f3670ff0f7883d3906777eba4e80aa692a218
Boris <boryanski213@gmail.com>
2022-09-06 21:45:52 +0300

    Создал метод ввода в массив строк

--- task.cs
+++ task.cs
@@ -1,4 +1,13 @@
 ﻿Console.Write("Введите размер массива(количество вводимых Вами слов/цифр и прочее: ");
 int len = Convert.ToInt32(Console.ReadLine());
 
+void FillArray(string[] array) {
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write("Введите строку #{0}: ", i + 1);
+      array[i] = Console.ReadLine();
+   }
+}
+
 string[] array = new string[len];
+FillArray(array);
