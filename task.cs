aa509641d92d99250b04134ba5e1861bc0656ab4
Boris <boryanski213@gmail.com>
2022-09-06 21:46:44 +0300

    Создал метод печати изначального массива

--- task.cs
+++ task.cs
@@ -9,5 +9,16 @@ void FillArray(string[] array) {
    }
 }
 
+void PrintArray(string[] array) {
+   Console.Write("[ ");
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write($"'{array[i]}' ");
+   }
+   Console.Write("] -> ");
+}
+
 string[] array = new string[len];
 FillArray(array);
+Console.WriteLine();
+PrintArray(array);
